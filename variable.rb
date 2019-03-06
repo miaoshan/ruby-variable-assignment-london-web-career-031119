879d376f007203544588ec4ca6ca6db6f6a3bdb8
github username <github email address>
2019-03-06 18:01:54 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,4 +1,5 @@
-def greeting="Hello World"
+greeting="Hello World"
 
-end # Define a local variable called 'greeting',
+
+ # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
